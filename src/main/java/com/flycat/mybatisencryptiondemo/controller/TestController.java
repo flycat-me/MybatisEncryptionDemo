82b4cb624d70c14d303d8c3d11c5cb9e32f260f3
tangzhuangzhi <tangzhuangzhi@eorionsolution.com>
2022-07-28 17:39:55 +0800

    增加web模块

--- src/main/java/com/flycat/mybatisencryptiondemo/controller/TestController.java
+++ src/main/java/com/flycat/mybatisencryptiondemo/controller/TestController.java
@@ -1,5 +1,23 @@
 package com.flycat.mybatisencryptiondemo.controller;
 
 
+import com.flycat.mybatisencryptiondemo.entity.EncryptedData;
+import com.flycat.mybatisencryptiondemo.mapper.EncryptedDataMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v2")
 public class TestController {
+
+    @Autowired
+    private EncryptedDataMapper mapper;
+
+    @GetMapping("/one")
+    public EncryptedData getByID(@RequestParam("id") int id){
+        return mapper.getEncrypted(id);
+    }
 }
